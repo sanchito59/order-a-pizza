0cee303d3dcc9fae8f5ceab31cf4fd4759e01f8f
Chris Sanchez <c.sanch7@gmail.com>
2019-11-08 09:19:57 -0800

    add functioning pizza object, no ability to calculate price based on toppingArray yet

--- js/scripts.js
+++ js/scripts.js
@@ -8,24 +8,38 @@ Pizza.prototype.getPrice = function () {
     this.toppings.forEach(function(topping){
         this.price += 0.75;
     }) 
-    if(this.size === 'Large'){
+    if(this.size === 9){
         this.price += 9;
-    } else if(this.size === 'Medium'){
+    } else if(this.size === 7){
         this.price += 7;
-    } else if(this.size === 'Personal'){
+    } else if(this.size === 5){
         this.price += 5;
     } else {
         console.log('Please pick a size.');       
     }
+    return this.price;
 }
 
 
 $(document).ready(function () {
     $('#orderForm').submit(function (event) {
+        let toppingArray = [];
         event.preventDefault();
         console.log('Order Up!');
         const sizeSelect = parseInt($('option:selected').val());
+        const toppingSelect = $('input[name="topping"]:checked').each(function(){
+            toppingArray.push($(this).val());
+        });
+        toppingArray.push(toppingSelect);
+        console.log(toppingArray);
         console.log(sizeSelect);
+        console.log(toppingSelect.length);
+
+        let pizza = new Pizza(sizeSelect, toppingArray);
+        let price = pizza.getPrice();
+        console.log(pizza);
+        console.log(price);
+  
         
         
 
