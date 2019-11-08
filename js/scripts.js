bd58fd7ec6de488851719d07a310bfc948c59cfc
Chris Sanchez <c.sanch7@gmail.com>
2019-11-08 08:01:56 -0800

    initialize repo with starter template

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function Construction() {
+    this. = ;
+}
+
+Construction.prototype.method = function () {
+    
+}
+
+
+$(document).ready(function () {
+    $('#formId').submit(function (event) {
+        event.preventDefault();
+
+    })
+});
