9e9a1eab0f355a3d22fc2fef8fd380841df30b7f
Chris Sanchez <c.sanch7@gmail.com>
2019-11-08 08:11:04 -0800

    add content to index.html and add constructor and prototype to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
-function Construction() {
-    this. = ;
+function Pizza(size, toppings) {
+    this.size = size,
+    this.toppings = toppings,
+    this.price = 0
 }
 
-Construction.prototype.method = function () {
-    
+Construction.prototype.getPrice = function () {
+    this.toppings.forEach(function(topping){
+        this.price += 1;
+    }) 
+    if(this.size === 'Large'){
+        this.price += 7;
+    } else if(this.size === 'Medium'){
+        this.price += 5;
+    } else if(this.size === 'Personal'){
+        this.price += 3;
+    } else {
+        console.log('Please pick a size.');       
+    }
 }
 
 
 $(document).ready(function () {
-    $('#formId').submit(function (event) {
+    $('#orderForm').submit(function (event) {
         event.preventDefault();
 
     })
